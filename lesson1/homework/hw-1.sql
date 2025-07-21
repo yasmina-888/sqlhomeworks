90e173886886996f40ab3a92b9af1bbb9e26fdee
yasmina-888 <dosmetovay@gmail.com>
2025-07-21 17:06:53 +0500

    Create hw-1.sql

---
+++ lesson1/homework/hw-1.sql
@@ -0,0 +1,15 @@
+create database School
+
+use School
+
+create table Students (StudentId int, Name varchar(50), Age int)
+select*from Students
+insert into Students values (1,'Umida',25),(2,'MuhammadAli',24),(3,'Shoyad',27)
+drop table students
+
+ SSMS (SQL Server Management Studio) 
+ SQL (Structured Query Language)
+
+What are the different authentication modes available when connecting to SQL Server? (Give at least 2)
+1. Windows Authentication
+2.SQL Server Authentication
